b59f76b955fb9f54aecbaf0ba6f61d6bd7c58bf4
Fragkiskos <fragkiskos/katsimpas@comparethemarket.com>
2019-01-13 18:48:30 +0000

    Add FitNesse

---
+++ AcceptanceTests/AcceptanceTests-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  AcceptanceTests-Bridging-Header.h
+//  OCSlimProject
+//
+//  Created by OCSlimProject on 13/01/2019.
+//  MIT License
+//  © 2016 Paul Stringer
+//
+
+
+
+#import "SlimTables.h"
---
+++ Podfile
@@ -0,0 +1,6 @@
+use_frameworks!
+
+ target 'AcceptanceTests' do
+    platform :ios, '10.0'
+    pod 'OCSlimProject', '~> 1.3'
+ end
